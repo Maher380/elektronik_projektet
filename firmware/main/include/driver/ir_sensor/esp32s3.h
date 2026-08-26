5cfae2e7ba098c9f93c1eb5c092713af30391cf2
Maher Darwesh <mdarwish925@gmail.com>
2026-08-26 10:40:28 +0200

    IR-sensor drivers

---
+++ firmware/main/include/driver/ir_sensor/esp32s3.h
@@ -0,0 +1,46 @@
+/**
+ * @file IR sensor ESP32-S3 driver interface.
+ */
+
+#pragma once 
+
+#include "driver/ir_sensor/interface.h"
+#include "driver/adc/interface.h"
+
+namespace driver::ir_sensor
+{
+
+class Esp32s3 final : public Interface
+{
+
+public:
+    
+    /**
+     * @brief Constructor.
+     * 
+     * @param[in] adc Reference to an ADC-driver interface.
+     */
+    Esp32s3(driver::adc::Interface& adc) noexcept ;
+
+    /**
+     * @brief Destructor.
+     */
+    ~Esp32s3() noexcept override = default;
+
+    /**
+     * @brief Read the current distance from the sensor.
+     * 
+     * @return Distance in cm
+     */
+    float readDistance() noexcept override;
+
+    /**
+     * @brief Check if sensor is running.
+     * 
+     * @return True if enabled, false otherwise.
+     */
+    bool isInitialized() const noexcept override;
+
+    private : driver::adc::Interface &myAdc;
+};
+} // namespace driver::ir_sensor
---
+++ firmware/main/include/driver/ir_sensor/interface.h
@@ -0,0 +1,36 @@
+/**
+ * @file IR sensor interface.
+ */
+
+#pragma once 
+
+
+namespace driver::ir_sensor
+{
+
+class Interface
+{
+
+public:
+    
+    /**
+     * @brief Destructor.
+     */
+    virtual ~Interface() noexcept = default;
+
+    /**
+     * @brief Read the current distance from the sensor.
+     * 
+     * @return Distance in cm
+     */
+    virtual float readDistance() noexcept = 0;
+
+    /**
+     * @brief Check if sensor is running.
+     * 
+     * @return True if enabled, false otherwise.
+     */
+    virtual bool isInitialized() const noexcept = 0;
+
+};
+} // namespace driver::ir_sensor
